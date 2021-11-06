5fcb76a989a42854a3141f356b88168d2cc7b56e
telio-s <telio.s@hotmail.com>
2021-11-06 21:17:26 +0700

    demo gif

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Monster");
+        primaryStage.setTitle("Stranger Tong");
         primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
     }
